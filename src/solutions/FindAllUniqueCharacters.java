2db2c544f61e0fe6640074ed2794982df7639c9b
audwit <audwit100@gmail.com>
2017-02-07 17:35:40 -0600

    Wrote a method to find if there are any unique characters in a string. Solved Cgapter 1, problem 1.1

---
+++ src/solutions/FindAllUniqueCharacters.java
@@ -0,0 +1,34 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class FindAllUniqueCharacters
+{
+	public boolean hasUniqueCharacters1(String str)
+	{
+		boolean[] checker = new boolean[256];
+		for(int i=0; i<str.length(); i++)
+		{
+			int val = str.charAt(i);
+			if(checker[val])
+				return false;
+			else
+				checker[val]=true;
+		}
+		return true;
+	}
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String inputString = scanner.nextLine();
+		FindAllUniqueCharacters findAllUniqueCharacters = new FindAllUniqueCharacters();
+		if(findAllUniqueCharacters.hasUniqueCharacters1(inputString))
+			System.out.println("All of the characters are unique");
+		else
+			System.out.println("There are duplicate characters");
+//		int val = inputString.charAt(0);
+//		System.out.println(val);
+	}
+
+}
