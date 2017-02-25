ea1d0f243be4e9411243983339482d5e4c43a5fb
audwit <audwit100@gmail.com>
2017-02-25 14:10:54 -0600

    Solved reverse string problem 1.2

---
+++ src/solutions/ReverseString.java
@@ -0,0 +1,34 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class ReverseString
+{
+	public void reverseStringInput(String input)
+	{
+		char[] x = input.toCharArray();
+		char y;
+		int length = input.length();
+		
+			for(int i=0; i<=length/2-1;i++)
+			{
+				y=input.charAt(i);
+				x[i] = x[length-1-i];
+				x[length-1-i] = y;
+			}
+		
+		for(int i=0; i<x.length;i++)
+			System.out.println(x[i]);
+	}
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner scanner = new  Scanner(System.in);
+		String input = scanner.nextLine();
+		
+		ReverseString rs = new ReverseString();
+		rs.reverseStringInput(input);
+	}
+
+}
